ac7ba436566ea1588c5a55674af5fd279a28ff49
irsyad <1rsy4d.m@gmail.com>
2021-03-08 18:04:26 +0700

    Added Sound and Animation
    Added Sound for when you smashed the zombies
    Added blood effect and camera shake

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public AudioClip[] smashSounds;
+    AudioSource audioSource;
+    public GameObject bloodEffect;
+
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,9 +31,17 @@ public class PlayerInput : MonoBehaviour
                 if(hit.collider.tag == "Enemy")
                 {
                     //Code to kill enemy
+                    audioSource.PlayOneShot(smashSounds[Random.Range(0, smashSounds.Length)], 0.1f);
                     gameObject.GetComponent<GameManager>().killEnemy();
+                    Camera.main.GetComponent<Animator>().SetTrigger("Shake");
+                    DisplayBloodEffect(Camera.main.ScreenToWorldPoint(Input.mousePosition));
                 }
             }
         }
+    }
+    private void DisplayBloodEffect(Vector2 pos)
+    {
+        bloodEffect.transform.position = pos;
+        bloodEffect.GetComponent<Animator>().SetTrigger("Smashed");
     }
 }
